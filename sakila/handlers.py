c11b7c16c33701458d00cd3a7e55c63aaa70046d
hirokiky <hirokiky@gmail.com>
2013-04-29 08:33:32 +0900

    Deleted a unnecessary file.

--- sakila/handlers.py
+++
@@ -1,8 +0,0 @@
-import decimal
-
-def highcharts_linechart_handler(obj):
-    if hasattr(obj, 'timetuple'):
-        return 'Date.UTC(%s, %s, %s)' % obj.timetuple()[:3]
-    if isinstance(obj, decimal.Decimal):
-        return float(obj)
-    return obj
