b69a52c4eb5ddef8f8a3a208935c37c5aa80a3ee
hirokiky <hirokiky@gmail.com>
2013-05-18 10:53:19 +0900

    Added newfound view for appending slash.

--- sakila/views.py
+++ sakila/views.py
@@ -1,5 +1,11 @@
 #! -*- coding:utf-8 -*-
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.view import notfound_view_config, view_config
+
+
+@notfound_view_config(append_slash=True)
+def notfound(request):
+    return HTTPNotFound('Not found, bro.')
 
 
 @view_config(
