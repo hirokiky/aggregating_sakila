2dab184f36d071f7b61a9aa68912c3292d4474ff
hirokiky <hirokiky@gmail.com>
2013-05-18 16:38:04 +0900

    Moved to Highchartable translating process to linechart.Linechart.

--- sakila/tochart/linechart.py
+++ sakila/tochart/linechart.py
@@ -1,6 +1,12 @@
+from js.highcharts import highcharts
+
+from highcharts import Chart
+from highcharts.config_sections import ChartConfig, XAxisConfig
+from highcharts.series import LineSeries
 from zope.interface import implementer
 
 from .interfaces import ILinechart
+from .utils import datetime_to_timestamp, first_of
 
 @implementer(ILinechart)
 class Linechart(object):
@@ -14,3 +20,21 @@ class Linechart(object):
 
     def getY(self):
         return [y for x, y in self.context]
+
+    @property
+    def highchartable(self,
+                      renderTo='container',
+                      pointInterval=24 * 3600000):
+        highcharts.need()
+
+        series = LineSeries(data=self.getY(),
+                            pointInterval=pointInterval,
+                            pointStart=datetime_to_timestamp(first_of(self.getX())))
+        chart_config = ChartConfig(renderTo=renderTo)
+        xaxis_config = XAxisConfig(type='datetime',
+                                   maxZoom=len(self.context) * pointInterval)
+        chart = Chart(chart=chart_config,
+                      xAxis=xaxis_config)
+        chart.add_series(series)
+
+        return str(chart)
